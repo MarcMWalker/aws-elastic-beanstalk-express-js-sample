d3f32c83fc77da55df1087e8cb4e544af8823433
SomePointlessUsername <mmwalker1986@gmail.com>
2020-09-28 00:54:52 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello there Marc!'));
+app.get('/', (req, res) => res.send('Hello there Marc AGAIN!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
